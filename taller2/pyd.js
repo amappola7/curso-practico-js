824602daebc4dec1eabd402526d07204e8f027e2
Amappola <anam.porrasp@gmail.com>
2022-05-13 11:11:13 -0500

    Modificaciones

--- taller2/pyd.js
+++ taller2/pyd.js
@@ -1,4 +1,20 @@
-function calculoDescuento(precio, descuento) {
-    total = (precio*(100-descuento))/100;
-    console.log(total)
+// Calcular el precio después de descuentos
+function calcularTotal(precio, descuento) {
+    precio = Number(precio);
+    descuento = Number(descuento);
+    total = ((precio*(100-descuento))/100).toFixed(2);
+    return total;
 }
+
+// Mostrar el resultado en la web
+function showTotal(){
+    const inputPrecio = document.getElementById("precio");
+    const valuePrecio = inputPrecio.value;
+    const inputDescuento = document.getElementById("descuento");
+    const valueDescuento = inputDescuento.value;
+    const calculo = calcularTotal(valuePrecio, valueDescuento);
+    const showScreen = document.getElementById("showInScreen");
+    showScreen.innerText = `En total debes pagar $${calculo}`;
+}
+
+
